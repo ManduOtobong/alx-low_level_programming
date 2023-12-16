b9aaffe3fe869754008705d606d104bc20959fb7
ManduOtobong <mandubassey2015@gmail.com>
2023-12-15 20:43:17 -0800

    100 operations

---
+++ 0x18-dynamic_libraries/basic_operations.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+
+/**
+* add: function to add two numbers
+* @a: first number
+* @b: second number
+* Description: addition of a and b
+*
+* Return: addition result
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+* sub: function to subtract two numbers
+* @a: first number
+* @b: second number
+* Description: subtraction of a and b
+*
+* Return: subtraction result
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+* mul: function to multiply two numbers
+* @a: first number
+* @b: second number
+* Description: multiplication of a and b
+*
+* Return: multiplication result
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+* div: function to divide two numbers
+* @a: first number
+* @b: second number
+* Description: division of a and b
+*
+* Return: division result
+*/
+
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+* mod: function to calculate the modulus of two numbers
+* @a: first number
+* @b: second number
+* Description: modulus of a and b
+*
+* Return: modulus result
+*/
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
